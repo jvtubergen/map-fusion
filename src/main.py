e9cc6a888ae4fc05a0d9f607f5175894f74d34a5
jvtubergen <jvtubergen@pm.me>
2024-05-23 19:57:16 +0200

    nearest node + improve extract graph utility function
    nearest node is actually a function provided by OSMnx
    with `nearest_nodes`...

--- src/main.py
+++ src/main.py
@@ -74,11 +74,62 @@ names = [
  'roadster_athens'
 ]
 
-# Extract nodes from a graph into the format `{id, x, y}`.
-# Extract nodes from a graph into the format `{id: nparray(x,y)}`.
+# Extract nodes from a graph into the format `(id, nparray(x,y))`.
 def extract_nodes(G):
-    # return [{"id":node, "x":data['x'], "y":data['y']} for node, data in G.nodes(data = True)]
     return [( node, np.asarray([data['x'], data['y']], dtype=np.float64, order='c') ) for node, data in G.nodes(data = True)]
+
+# Extract nodes from a graph as a dictionary `{nid: nparray([x,y])}`.
+def extract_nodes_dict(G):
+    d = {}
+    for node, data in G.nodes(data = True):
+        d[node] = np.asarray([data['x'], data['y']], dtype=np.float64, order='c')
+    return d
+
+# Seek nearest vertex in graph of a specific coordinate of interest.
+# Expect point to be a 2D numpy array.
+def nearest_point(G, p):
+    # Example (extracting nearest vertex):
+    # nearest_point(extract_graph("chicago"), np.asarray((4.422440 , 46.346080), dtype=np.float64, order='c'))
+    points = extract_nodes(G)
+
+    # Seek nearest point
+    dmin = 1000000
+    ires = None
+    qres = None
+    for (i, q) in points:
+        m = np.linalg.norm(p - q)
+        if m < dmin:
+            dmin = m
+            (ires, qres) = (i, q)
+
+    return (ires, qres)
+
+# Utility function for convenience to extract graph by name.
+#   Either construction from raw data in folder or reading from graphml file.
+#   Expect folders with raw data to exist at "data/maps", further with properties described by `construct_graph`.
+#   Will either read and/or store from "graphs/" data folder.
+def extract_graph(name, reconstruct=False):
+    graphml_path = "graphs/"+name+".graphml"
+
+    if Path(graphml_path).exists() and not reconstruct:
+        G = ox.load_graphml(filepath=graphml_path)
+    else:
+        G = construct_graph("data/maps/" + name)
+        ox.save_graphml(G, filepath=graphml_path)
+    
+    return G
+
+
+# The names of the graphs we currently support, thus graphs we can work with.
+names = [
+ 'athens_large',
+ 'athens_small',
+ 'berlin',
+ 'chicago',
+ 'athens_large_kevin',
+ 'chicago_kevin',
+ 'roadster_athens'
+]
 
 # Seek nearest vertex in graph of a specific coordinate of interest.
 # Expect point to be a 2D numpy array.
