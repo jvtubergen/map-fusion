c6cdb6caeb3afc46c168d445d898387ffad44349
jvtubergen <jvtubergen@pm.me>
2024-06-05 13:30:39 +0200

    Deduplicating vectorized graph

--- src/main.py
+++ src/main.py
@@ -32,10 +32,35 @@ def duplicated_nodes(G):
             duplicated.append(node_id)
     return duplicated
 
+
+# Group duplicated nodes.
+def duplicated_nodes_grouped(G):
+    # NOTE: Keep IDs (integeters) separate from coordinates (floats): Numpy arrays all have same type.
+    node_ids = G.nodes()
+    coordinates = np.array([[info["x"], info["y"]] for node, info in G.nodes(data=True)])
+    uniques, inverses, counts = np.unique( coordinates, return_inverse=True, axis=0, return_counts=True )
+    # Construct dictionary.
+    duplicated = {}
+    for node_id, index_to_unique in zip(node_ids, inverses):
+        if counts[index_to_unique] > 1:
+            if index_to_unique in duplicated.keys():
+                duplicated[index_to_unique].append(node_id)
+            else:
+                duplicated[index_to_unique] = [node_id]
+    # Convert dictionary into a list.
+    result = []
+    for v in duplicated:
+        result.append(duplicated[v])
+    return result
+
+
 # Return edge IDs which are duplicated (exact same coordinates).
 # NOTE: Expects vectorized graph.
 # TODO: Group togeter edge-IDs that are duplicates of one another as tuples.
 def duplicated_edges(G):
+
+    if type(G) != nx.MultiDiGraph:
+        raise BaseException("Expect to call duplicated_edge_grouped on an nx.MultiDiGraph.")
 
     if G.graph["simplified"]:
         raise BaseException("Duplicated edges function is supposed to be called on a vectorized graph.")
@@ -59,6 +84,45 @@ def duplicated_edges(G):
         if counts[index_to_unique] > 1:
             duplicated.append(edge_id)
     return duplicated
+
+
+# Group duplicated edges.
+# NOTE: Expects MultiDiGraph
+def duplicated_edges_grouped(G):
+
+    if type(G) != nx.MultiDiGraph:
+        raise BaseException("Expect to call duplicated_edge_grouped on an nx.MultiDiGraph.")
+
+    if G.graph["simplified"]:
+        raise BaseException("Duplicated edges function is supposed to be called on a vectorized graph (thus not simplified).")
+
+    # NOTE: Keep IDs (integeters) separate from coordinates (floats): Numpy arrays all have same type.
+    edge_ids = G.edges(keys=True)
+    coordinates = []
+    for (a, b, k, attrs) in G.edges(keys=True, data=True):
+        x1 = G.nodes[a]["x"]
+        y1 = G.nodes[a]["y"]
+        x2 = G.nodes[b]["x"]
+        y2 = G.nodes[b]["y"]
+        coordinates.append([x1,y1,x2,y2])
+    coordinates = np.array(coordinates)
+
+    # Construct dictionary.
+    uniques, inverses, counts = np.unique( coordinates, return_inverse=True, axis=0, return_counts=True )
+    duplicated = {}
+    for edge_id, index_to_unique in zip(edge_ids, inverses):
+        if counts[index_to_unique] > 1:
+            if index_to_unique in duplicated.keys():
+                duplicated[index_to_unique].append(edge_id)
+            else:
+                duplicated[index_to_unique] = [edge_id]
+
+    # Convert dictionary into a list.
+    result = []
+    for v in duplicated:
+        result.append(duplicated[v])
+
+    return result
 
 
 # Uniform add edge curvature attribute to every edge to afterwards allow/simplify comparisons.
@@ -103,19 +167,14 @@ def extract_selfloop_graph(G):
 
 
 # Convert edge geometry (LineString) into a list of coordinates.
-def convert_geometry_from_attributes(attrs):
+# With `as_string` Convert edge geometry (LineString) into its serialized String.
+def extract_geometry_from_attributes(attrs, as_string=False):
     if "geometry" in attrs.keys():
+        if as_string:
+            return attrs["geometry"].wkt
         return list(attrs["geometry"].coords)
     else:
         return [[]]
-
-
-# Convert edge geometry (LineString) into its serialized String.
-def extract_geometry_from_attributes(attrs):
-    if "geometry" in attrs.keys():
-        return attrs["geometry"].wkt
-    else:
-        return ""
 
 
 # Rendering duplicated nodes and edges.
@@ -156,13 +215,61 @@ def node_by_coord(G, coord):
     return nodes[indices[0][0]]
 
 
+def isolated_nodes(G):
+    return [node_id for node_id in G.nodes() if len(G[node_id]) == 0]
+
+
+# Example (removing isolated nodes in G):
+# G.remove_nodes_from(isolated_nodes(G))
+   
+
+
+# Remove duplicated nodes and edges from vectorized graph.
+# NOTE: Since a vectorized only stores directly
+#       No need to 
+def deduplicate_vectorized_graph(G):
+
+    G = G.copy()
+    G = nx.Graph(G)
+    # Assert G is a Graph (unidirectional and single path)
+
+    # Deduplicate nodes: Adjust edges of deleted nodes + Delete nodes.
+    for group in duplicated_nodes_grouped(G):
+        base = group[0]
+        # for n, nbrs in G.adj.items():
+        #     # n: from node id
+        #     # nbrs: {<neighboring node id>: {<edge-id-connecting n with nbr>: <edge attributes>}}]
+        for remove in group[1:]:
+            # Replace edges
+            nbrs = G.adj[remove]
+            to_delete = []
+            to_add = []
+            for nbr in nbrs:
+                edge = G[remove][nbr]
+                to_delete.append((remove,nbr))
+                to_add.append((base,nbr, *edge))
+            # Delete and add in a single go to prevent changing the nbrs loop elements.
+            G.remove_edges_from(to_delete)
+            G.add_edges_from(to_add)
+            
+            # Expect remove node to be isolated now.
+            assert len(G[remove]) == 0
+            G.remove_node(remove)
+    
+    # Deduplicate edges:
+    # Since G is a graph, adding the edges already resolves into edge deduplication.
+    G = nx.MultiDiGraph(G) # Convert back into MultiDiGraph.
+    
+    return G
+
 
 # Example (vectorize and simplify again):
 # Note: It contains a bug: bidirectional self-loops are incorrectly removed.
-# G = extract_graph("chicago_kevin")
-# G2 = vectorize_graph(G)
-# G3 = ox.simplify_graph(G2)
-# G5 = ox.simplify_graph(vectorize_graph(G3))
+G = extract_graph("chicago_kevin")
+G2 = vectorize_graph(G)
+G2 = deduplicate_vectorized_graph(G2)
+G3 = ox.simplify_graph(G2)
+G5 = ox.simplify_graph(vectorize_graph(G3))
 # # Simple check on vectorization validity.
 # assert len(G.nodes()) == len(G3.nodes())
 # assert len(G.edges()) == len(G3.edges())
