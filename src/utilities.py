8596c7fae0390738cea4c9ce9113ef200925a400
jvtubergen <jvtubergen@pm.me>
2025-01-13 13:05:18 +0100

    Refactor: Curve cut at percentage.
    Significantly simplified by leveraging the curve cut by intervals function.

--- src/utilities.py
+++ src/utilities.py
@@ -73,24 +73,7 @@ def curve_length(ps):
     return sum([norm(p1 - p2) for p1, p2 in zip(ps, ps[1:])])
 
 # Cut a curve at a percentage (interval of [0, 1]).
-def curve_cut(ps, percentage):
-    assert len(ps) >= 2
-    steps = [norm(p1 - p2) for p1, p2 in zip(ps, ps[1:])]
-    length = sum(steps)
-    target = percentage * length
-    assert length > 0
-    current = 0
-    i = 0
-    while True:
-        step = steps[i]
-        upcoming = current + step
-        if upcoming > target: # Cut current edge.
-            line_percentage = (target - current) / step
-            p = line_percentage * ps[i+1] + (1 - line_percentage) * ps[i]
-            left  = np.append(ps[:i+1], [p], axis=0)
-            right = np.append([p], ps[i+1:], axis=0)
-            return (left, right)
-        current = upcoming
+curve_cut = lambda ps, percentage: curve_cut_intervals(ps, [percentage])
 
 # Cut curve in half.
 curve_cut_in_half = lambda ps: curve_cut(ps, 0.5)
