701f9c562d074683f602cf91667691181f9865f0
jvtubergen <jvtubergen@pm.me>
2025-01-15 10:08:25 +0100

    Refactor: Import locally written APLS and ignore external one.

--- src/internal.py
+++ src/internal.py
@@ -2,10 +2,6 @@
 import handlers.sat2graph as sat2graph
 import handlers.roadster as roadster
 import handlers.gmaps.lib as gmaps
-from apls.apls import apls_detailed, apls, apls_prime
-from apls.topo_metric import compute_topo as topo
-def topo_prime(truth, proposed, **kwargs):
-    return topo(truth, proposed, **kwargs, prime=True)
 
 from coordinates import *
 from utilities import *
@@ -20,6 +16,8 @@ from graph_coordinates import *
 from graph_curvature import *
 from graph_coverage import *
 from graph_merging import *
+
+from apls import *
 
 from network import *
 
