48b69b54c145e84e525aa05cf5fbe8e15e723858
jvtubergen <jvtubergen@pm.me>
2025-01-20 12:29:35 +0100

    Refactor: Moving sanity check logic and remove APLS preprocessing function since this is done by apls module now.

--- src/network.py
+++ src/network.py
@@ -45,19 +45,6 @@ def gen_random_shortest_path(G):
     return path
 
 
-#######################################
-### Task specific graph preprocessing
-#######################################
-
-# Preparing graph for APLS usage (simplified, multi-edge, all edges have geometry property, all edges have an edge length property).
-def graph_prepare_apls(G):
-    G = simplify_graph(graph_transform_latlon_to_utm(G))
-    G = graph_annotate_edge_length(G)
-    G = graph_add_geometry_to_straight_edges(G) # Add geometry for straight line segments (edges with no curvature).
-    # G.graph['crs'] = "EPSG:4326" # Set EPSG might be necessary for plotting results within APLS logic.
-    return G
-
-
 # Preparing graph for TOPO usage (simplified, multi-edge, all edges have geometry property, all edges have an edge length property).
 def graph_prepare_topo(graph):
     # t0 = time()
@@ -68,80 +55,3 @@ def graph_prepare_topo(graph):
     graph = graph_annotate_edge_length(graph)
     # verbose_print(f"Simplified graph in {int(time() - t0)} seconds.")
     return graph
-
-
-#######################################
-### Sanity check functionality
-#######################################
-
-# Perform a few sanity checks on the graph to prevent computation errors down the line.
-def graph_sanity_check(G):
-    print("Check graph.")
-
-    # Simplification.
-    if "simplified" not in G.graph:
-        raise Exception("Expect 'simplified' dictionary key in graph.")
-
-    # If simplified, then multigraph.
-    if G.graph["simplified"] and type(G) != type(nx.MultiGraph()):
-        raise Exception("Expect simplified graph to be an undirected multi-graph.") 
-    if not G.graph["simplified"] and type(G) != type(nx.Graph()):
-        raise Exception("Expect vectorized graph to be a undirected single-graph.") 
-
-    # Coordinates.    
-    if "coordinates" not in G.graph:
-        raise Exception("Expect 'coordinates' dictionary key in graph.")
-    if G.graph["coordinates"] != "utm" and G.graph["coordinates"] != "latlon":
-        raise Exception("Expect 'coordinates' dictionary value to be either 'utm' or 'latlon'.")
-
-    # Nodes.
-    nodes = extract_node_positions(G)
-    if G.graph["coordinates"] == "utm":
-        if np.min(nodes) < 100: 
-            print(nodes)
-            raise Exception("Expect graph in utm coordinate system. Big chance some node is in latlon coordinate system.")
-    if G.graph["coordinates"] == "latlon":
-        if np.min(nodes) > 100: 
-            print(nodes)
-            raise Exception("Expect graph in latlon coordinate system. Big chance some node is in utm coordinate system.")
-
-    # Node (x,y coordinates) flipping.
-    coord0 = np.min(nodes, axis=0)
-    coord1 = np.max(nodes, axis=0)
-    diffa = np.max(nodes[:,0]) - np.min(nodes[:,0])
-    diffb = np.max(nodes[:,1]) - np.min(nodes[:,1])
-    # print(diffa, diffb)
-    diffc = np.max(nodes[:,0]) - np.min(nodes[:,1])
-    diffd = np.max(nodes[:,1]) - np.min(nodes[:,0])
-    # print(diffc, diffd)
-
-    if G.graph["coordinates"] == "latlon" and (abs(diffa) > 1 or abs(diffb) > 1):
-        print(nodes)
-        print(abs(diffa), abs(diffb))
-        raise Exception("Expect node y, x coordinate consistency.") 
-    if G.graph["coordinates"] == "utm" and (abs(diffa) > 100000 or abs(diffb) > 100000):
-        print(nodes)
-        print(abs(diffa), abs(diffb))
-        raise Exception("Expect node y, x coordinate consistency.") 
-    
-    # Edges.
-    nodes = extract_nodes_dict(G)
-    if G.graph["simplified"]: 
-        for (a, b, k, attrs) in G.edges(data=True, keys=True):
-            ps = edge_curvature(G, a, b, k)
-            if G.graph["coordinates"] == "latlon": # Convert to utm for computing in meters.
-                ps = array([latlon_to_coord(latlon) for latlon in ps])
-            if curve_length(ps) > 1000: # Expect reasonable curvature length.
-                raise Exception("Expect edge length less than 1000 meters. Probably some y, x coordinate in edge curvature got flipped.")
-            # Expect start and endpoint of edge curvature match the node position.
-            ps = edge_curvature(G, a, b, k) # Expect startpoint matches curvature.
-            try:
-                if (not np.all(ps[0] == nodes[a])) and (not np.all(ps[-1] != nodes[b])):
-                    raise Exception("Expect curvature have same directionality as edge start and end edge.")
-            except Exception as e:
-                print(traceback.format_exc())
-                print(e)
-                breakpoint()
-            
-            assert "geometry" in attrs
-            assert "curvature" in attrs
