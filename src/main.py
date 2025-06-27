66b9216b94f54f62fcfd65503d40712e15576dde
jvtubergen <jvtubergen@pm.me>
2025-06-27 21:06:51 +0200

    Main: Drop debugging code, its done by running python.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,1 @@
 from workflow import *
-
-# Read for debugging variable.
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--debug', action='store_true', help='Enable debug mode')
-args = parser.parse_args()
-debug = args.debug
-
-# Start debugging by listening on port.
-if debug:
-    import debugpy
-    debugpy.listen(("localhost", 5678))
-    debugpy.wait_for_client()
