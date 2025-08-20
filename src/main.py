bfba3942f1112cec867f636943bec783f379834f
jvtubergen <jvtubergen@pm.me>
2025-08-20 16:22:04 +0000

    Main: Update experiment workflow runner

--- src/main.py
+++ src/main.py
@@ -1,4 +1,44 @@
 from workflow import *
 
-workflow_obtain_graphs()
-# maps = generate_maps()
+# Experiment 0: General information, edge coverage, and graph distance on base graphs.
+# experiment_zero_edge_coverage_base_graphs()
+# experiment_zero_graph_distances(sample_size=5000)
+
+
+# Experiment 1: Obtain map similarity table.
+# for inverse in [False, True]:
+#     obtain_fusion_maps(inverse=inverse)
+#     obtain_prepared_metric_maps(inverse=True)
+#     obtain_shortest_distance_dictionaries(inverse=inverse)
+# experiments_one_base_table("berlin",  threshold=30, sample_count=10000, prime_sample_count=2000)
+# experiments_one_base_table("chicago", threshold=30, sample_count=10000, prime_sample_count=2000)
+
+
+# Experiment 2: 
+# obtain_threshold_data(sample_count=50, prime_sample_count=10)
+# obtain_threshold_data(sample_count=500, prime_sample_count=100)
+# obtain_threshold_data(sample_count=2000, prime_sample_count=400)
+# experiment_two_threshold_performance(sample_count=50, prime_sample_count=10, inverse=False)
+# experiment_two_threshold_performance(sample_count=50, prime_sample_count=10, inverse=True)
+# experiment_two_threshold_performance(sample_count=500, prime_sample_count=100, inverse=False)
+# experiment_two_threshold_performance(sample_count=500, prime_sample_count=100, inverse=True)
+# experiment_two_threshold_performance(sample_count=2000, prime_sample_count=400, inverse=False)
+# experiment_two_threshold_performance(sample_count=2000, prime_sample_count=400, inverse=True)
+# experiment_two_basic_information()
+# experiment_two_threshold_impact_on_metadata()
+
+
+# Experiment 3:
+experiment_three_TOPO_hole_size(sample_count=2000, prime_sample_count=1000)
+# experiment_three_sample_distribution(sample_count=2000)
+# experiment_three_prime_sample_distribution(prime_sample_count=2000)
+
+
+
+# Qualitative:
+# plot_base_maps()
+# plot_IDR_maps()
+# plot_IDR_maps_with_actions()
+# plot_IDR_maps_with_actions(for_zoomed=True)
+# plot_IDR_maps_with_actions_at_extremes(place="berlin", low_threshold=1)
+# plot_IDR_maps_with_actions_at_extremes(place="chicago", low_threshold=1)
